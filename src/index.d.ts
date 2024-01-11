552be9b337900098cfff8d9c93a56b453864b7d4
ghulamyazdani <ghulamyazdani12@gmail.com>
2024-01-11 13:06:54 +0530

    Fix : issues in the build

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,4 +2,4 @@ import React from 'react';
 import { ButtonProps } from './components/Button/Button';
 
 // export const Button: (props: ButtonProps) => React.FC<ButtonProps>;
-export const Button: React.FC<ButtonProps>;
+export let Button: React.FC<ButtonProps>;
